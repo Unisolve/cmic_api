2127cad5b7f0d2f180886e0e5b9cabe49ed4dd0d
Simon Taylor <simon@unisolve.com.au>
2026-04-30 19:43:14 +1000

    Add financials/get_apcheque.sh test script for AP cheque GET endpoint

---
+++ bash/financials/get_apcheque.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# name:    get_apcheque.sh
+# process: Query an AP Cheque
+#          GET /ap-rest-api/rest/1/apcheque?limit=3&offset=0
+# docs:    https://developers.cmicglobal.com/apidocs/query-an-ap-cheque
+
+#
+# Load configuration
+#
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/../config"
+
+if [[ -z "$CMIC_USERNAME" || -z "$CMIC_PASSWORD" ]]; then
+    echo "Error: Configuration not loaded. Please copy config.template to config and set your credentials."
+    exit 1
+fi
+
+#
+# Run endpoint
+#
+
+curl -u "${CMIC_USERNAME}:${CMIC_PASSWORD}" \
+  --url "${CMIC_BASE_URL}/ap-rest-api/rest/1/apcheque?limit=3&offset=0" \
+  --header "accept: ${CMIC_ACCEPT_HEADER}"
